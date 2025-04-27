00f51e4a53d6d74191545f8dbd98239d2167c6fa
devRajeshThapa <devrajeshthapa@gmail.com>
2025-04-27 23:15:16 +0545

    Serve

--- src/controllers/mailController.mjs
+++ src/controllers/mailController.mjs
@@ -37,8 +37,8 @@ const sendMail = async ({type, payload}) => {
         if (error) {
             return console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // console.log('Message sent: %s', info.messageId);
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     });
 }
 
